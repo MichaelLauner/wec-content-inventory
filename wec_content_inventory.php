d446db8c546f4659a78e41a75dbedc65ba0992d2
Michael Launer <launer.michael@gmail.com>
2016-12-29 13:24:14 -0700

    Basic Start - Plugin Showing In List

---
+++ wec_content_inventory.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+    Plugin Name: WP Content Inventory
+    Plugin URI: http://www.westedge.us
+    Description: Plugin for managing content and building review processes for clients with lots of content
+    Author: Michael Launer - West Edge Collective
+    Version: 1.0
+    Author URI: http://www.westedge.us
+    */
+?>
